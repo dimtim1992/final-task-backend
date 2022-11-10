6af16cb7289caab4b68419d15565824fd94bf7b3
dimtim1992 <dimtim7272807@gmail.com>
2022-11-10 14:08:21 +0300

    refactor: change mongoose.connect param string

--- Project management application/src/index.ts
+++ Project management application/src/index.ts
@@ -6,7 +6,7 @@ import * as serverService from './services/server.service';
 
 (async () => {
   try {
-    await mongoose.connect('mongodb+srv://${DzmitryTsim}:${Dzmitry1992}@${cluster0.pmsahby}.mongodb.net/managerApp');
+    await mongoose.connect('mongodb+srv://DzmitryTsim:Dzmitry1992@cluster0.pmsahby.mongodb.net/managerApp');
     serverService.server.listen(process.env.PORT || PORT, function () {
       console.log('Сервер ожидает подключения...');
     })
